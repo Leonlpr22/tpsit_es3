ae327b18f062291d5fa01f8a4490b071e3f547c2
Leonlpr22 <leonleone2005@gmail.com>
2024-10-09 09:09:08 +0200

    server

--- src/main/java/lopreiato/it/Main.java
+++ src/main/java/lopreiato/it/Main.java
@@ -1,7 +1,17 @@
 package lopreiato.it;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        ServerSocket ss = new ServerSocket(5637);
+
+        Socket mySocket = ss.accept(); //istruzione bloccante (in attesa di un collegamento, se qualcuno si collega ritorna un socket)
+
+        System.out.println("Qualcuno si è collegato");
     }
 }
