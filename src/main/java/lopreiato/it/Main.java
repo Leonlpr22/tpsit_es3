c63fbf957300f51400f1bbf3eeb1e54ee0d56fc3
Leonlpr22 <leonleone2005@gmail.com>
2024-10-09 09:12:00 +0200

    server finito

--- src/main/java/lopreiato/it/Main.java
+++ src/main/java/lopreiato/it/Main.java
@@ -1,17 +1,32 @@
 package lopreiato.it;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
+        System.out.println("Server in avvio!");
 
         ServerSocket ss = new ServerSocket(5637);
 
         Socket mySocket = ss.accept(); //istruzione bloccante (in attesa di un collegamento, se qualcuno si collega ritorna un socket)
 
-        System.out.println("Qualcuno si è collegato");
+        System.out.println("Un client si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta:" + stringaRicevuta);
+
+        String stringaMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringaMaiuscola + '\n');
+
+        mySocket.close();
+        ss.close();
     }
 }
